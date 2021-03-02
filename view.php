cfd4f5027c6d82238e21f47a18ac505af47debba
DeepPatel2142 <deeppatel2592@gmail.com>
2021-03-02 13:16:35 -0500

    Update view.php

--- view.php
+++ view.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thanks for submitting</title>
-</head>
-<body>
-  <header>
-        <h1> TuneShare 2021 - Share Your Fave Tunes & Join The Community</h1>
-  </header>
+<?php require('header.php')?>
         <?php
         
         //connect to db
@@ -41,8 +31,4 @@
     
 
         ?>
-  <footer>
-        <p> &copy; <?php echo getdate()['year']; ?> </p>
-  </footer> 
-</body>
-</html>
+<?php require('footer.php')?>
